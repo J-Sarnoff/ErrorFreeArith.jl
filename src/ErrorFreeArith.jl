8534e9aaa106424840fce0b4532d9ec913cff17d
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-15 20:18:09 -0500

    Update ErrorFreeArith.jl

--- src/ErrorFreeArith.jl
+++ src/ErrorFreeArith.jl
@@ -2,7 +2,7 @@ module ErrorFreeArith
 
 export eftSum2, eftSum2inOrder, eftDiff2, eftDiff2inOrder, eftProd2, eftDiv2, eftRecip, eftSqrt
 
-if isdefined(:inline) && (:inline == true)
+if isdefined(:inlineErrorFreeArith) && (inlineErrorFreeArith == true)
    import("inlined.jl")
 else
    import("funcall.jl")
