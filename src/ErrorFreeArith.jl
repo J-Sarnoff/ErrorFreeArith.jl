4f2f4182c3c337c74357afb61ce8c6a4f9b6f6e8
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-25 07:31:10 -0500

    Update ErrorFreeArith.jl

--- src/ErrorFreeArith.jl
+++ src/ErrorFreeArith.jl
@@ -1,7 +1,8 @@
 module ErrorFreeArith
 
 export eftRecip, eftSquare, eftCube, eftSqrt, eftRecipSqrt, # see comment for eftSqrt
-       eftSum2, eftSum2inOrder, eftDiff2, eftDiff2inOrder, eftProd2, eftDiv2
+       eftSum2, eftSum2inOrder, eftDiff2, eftDiff2inOrder, eftProd2, eftDiv2,
+       eftSum3, eftSum3inOrder, eftProd3, eftFMA, eftFMS
 
 if isdefined(:inlineErrorFreeArith) && (inlineErrorFreeArith == true)
    include("inlined.jl")
