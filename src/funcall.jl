6ab1b1bdb111ffaab21f9efc849caa2152df85d1
J-Sarnoff <jeffreysarnoff@gmail.com>
2015-11-25 07:33:37 -0500

    Update funcall.jl

--- src/funcall.jl
+++ src/funcall.jl
@@ -1,42 +1,4 @@
-
-function eftSum2{T<:AbstractFloat}(a::T, b::T)
-  x = a + b
-  t = x - a
-  y = (a - (x - t)) + (b - t)
-  x,y
-end
-
-function eftSum2inOrder{T<:AbstractFloat}(a::T, b::T)
-  x = a + b
-  y = b - (x - a)
-  x,y
-end
-
-function eftDiff2{T<:AbstractFloat}(a::T, b::T)
-  x = a - b
-  t = x - a
-  y = (a - (x - t)) - (b + t)
-  x,y
-end
-
-function eftDiff2inOrder{T<:AbstractFloat}(a::T, b::T)
-  x = a - b
-  y = (a - x) - b
-  x,y
-end
-
-function eftProd2{T<:AbstractFloat}(a::T, b::T)
-    x = a * b
-    y = fma(a, b, -x)
-    x,y
-end
-
-# sassafrass!! 'y' must be negated to get the right result
-function eftDiv2{T<:AbstractFloat}(a::T,b::T)
-     x = a/b
-     y = -(fma(x,b,-a)/b)
-     x,y
-end
+#= single parameter error-free transformations =#
 
 # sassafrass!  'y' must be negated to get the right result
 function eftRecip{T<:AbstractFloat}(a::T)
@@ -78,7 +40,49 @@ function eftCube(a::Float64)
     x,y
 end
 
-#= three term error-free transformations =#
+#= two parameter error-free transformations =#
+
+function eftSum2{T<:AbstractFloat}(a::T, b::T)
+  x = a + b
+  t = x - a
+  y = (a - (x - t)) + (b - t)
+  x,y
+end
+
+function eftSum2inOrder{T<:AbstractFloat}(a::T, b::T)
+  x = a + b
+  y = b - (x - a)
+  x,y
+end
+
+function eftDiff2{T<:AbstractFloat}(a::T, b::T)
+  x = a - b
+  t = x - a
+  y = (a - (x - t)) - (b + t)
+  x,y
+end
+
+function eftDiff2inOrder{T<:AbstractFloat}(a::T, b::T)
+  x = a - b
+  y = (a - x) - b
+  x,y
+end
+
+function eftProd2{T<:AbstractFloat}(a::T, b::T)
+    x = a * b
+    y = fma(a, b, -x)
+    x,y
+end
+
+# sassafrass!! 'y' must be negated to get the right result
+function eftDiv2{T<:AbstractFloat}(a::T,b::T)
+     x = a/b
+     y = -(fma(x,b,-a)/b)
+     x,y
+end
+
+
+#= three parameter error-free transformations =#
 
 function eftSum3{T<:Float64}(a::T,b::T,c::T)
     s,t = eftSum2(b, c)
@@ -96,14 +100,12 @@ function eftSum3inOrder{T<:Float64}(a::T,b::T,c::T)
     x,y,z
 end
 
-
 function eftProd3{T<:Float64}(a::T, b::T, c::T)
     p,e = eftProd2(a,b)
     x,p = eftProd2(p,c)
     y,z = eftProd2(e,c)
     x,y,z
 end
-
 
 function eftFMA{T<:Float64}(a::T, b::T, c::T)
     x = fma(a,b,c)
